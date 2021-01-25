215a74557fabb9b1a78a1d6640116e0ea71440cb
Opilane TTHK <opilane@tthk.ee>
2021-01-25 13:57:15 +0200

    first commit

---
+++ CatsAndDogs/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CatsAndDogs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dogPicture = new System.Windows.Forms.PictureBox();
+            this.getADog = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dogPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dogPicture
+            // 
+            this.dogPicture.Location = new System.Drawing.Point(670, 137);
+            this.dogPicture.Name = "dogPicture";
+            this.dogPicture.Size = new System.Drawing.Size(382, 443);
+            this.dogPicture.TabIndex = 0;
+            this.dogPicture.TabStop = false;
+            // 
+            // getADog
+            // 
+            this.getADog.Location = new System.Drawing.Point(797, 664);
+            this.getADog.Name = "getADog";
+            this.getADog.Size = new System.Drawing.Size(98, 36);
+            this.getADog.TabIndex = 1;
+            this.getADog.Text = "Get Dog Image";
+            this.getADog.UseVisualStyleBackColor = true;
+            this.getADog.Click += new System.EventHandler(this.getADog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1202, 872);
+            this.Controls.Add(this.getADog);
+            this.Controls.Add(this.dogPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dogPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox dogPicture;
+        private System.Windows.Forms.Button getADog;
+    }
+}
+
